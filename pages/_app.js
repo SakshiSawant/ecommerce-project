168608a94c0eb74fa1ce88e6375a86db366a3e7c
sakshi192000 <sakshisawants4100@gmail.com>
2022-02-20 12:19:56 +0530

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps }) {
       </nav>  */}
 
 
-<nav class="flex items-center justify-between flex-wrap bg-blue-800 p-6">
+<nav class="flex items-center justify-between flex-wrap bg-sky-500 p-6">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
           {/* <!-- Image --> */}
           <Image
@@ -39,19 +39,19 @@ function MyApp({ Component, pageProps }) {
  
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+      <a href="/" class="block mt-4 lg:inline-block text-lg lg:mt-0 text-white hover:text-white mr-4">
         Home
       </a>
-      <a href="/create-item" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+      <a href="/create-item" class="block mt-4 text-lg lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
         Create Item
       </a>
-      <a href="/my-assets" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+      <a href="/my-assets" class="block mt-4 text-lg lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
         MyItem
       </a>
-      <a href="/creator-dashboard" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+      <a href="/creator-dashboard" class="block mt-4 text-lg lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
         Dashboard
       </a>
-      <a href="/transaction" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
+      <a href="/transaction" class="block mt-4 text-lg lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
         My Transactions
       </a>
     </div>
